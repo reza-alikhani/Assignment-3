50f7f4478836d380821bcd792affa7084e258c46
reza-alikhani <113200309+reza-alikhani@users.noreply.github.com>
2024-03-29 20:28:03 -0400

    Update Assignment 3_2.py

--- Assignment 3_2.py
+++ Assignment 3_2.py
@@ -5,6 +5,7 @@ n = random.randint(1, 100)
 numbers = []
 for i in range(n):
     x = random.randint(0, 100)
-    numbers.append (x)
+    if x not in numbers:
+        numbers.append (x)
     
 print(numbers)
