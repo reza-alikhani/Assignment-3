1eb8baeb5a3b905174dc062d67bc1303dfa2ff52
reza-alikhani <113200309+reza-alikhani@users.noreply.github.com>
2024-04-01 16:33:14 -0400

    Add files via upload

--- Assignment 3_5.py
+++ Assignment 3_5.py
@@ -1,4 +1,9 @@
-import math
-a=int(input("Enter your first number:"))
-b=int(input("Enter your second number:"))
-print("The greatest common divisor of the two numbers is:",math.gcd(a,b))
+first_number = int(input("Enter first number: "))
+second_number = int(input("enter second number: "))
+
+for i in range (1 , (min(first_number, second_number)+ 1)):
+    if first_number % i == 0 and second_number % i == 0:
+        gcd = i
+
+print("gcd =", gcd )
+
